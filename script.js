3dd17d99f13627813c7c1675dbb39592356eb558
timchang <changtimchang@protonmail.com>
2024-10-09 16:22:05 +0900

    html origin

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//banner slide
+$('.slick-carousal').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 80000,
+    arrows : true,
+    centerMode: true,
+    cneterPadding: '60px',
+    infinite: true,
+    prevArrow : '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow : '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></button>',
+    
+  });
